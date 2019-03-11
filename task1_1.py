256e0b22c344c76f50d3c4d6ea33caadb1fe45d4
Oleg Sapegin <sapegin.o@gmail.com>
2019-03-11 09:35:08 +0300

    homework6

---
+++ task1_1.py
@@ -0,0 +1,118 @@
+"""
+Вариант из ПЗ.
+6. В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.
+Сами минимальный и максимальный элементы в сумму не включать.
+"""
+import random
+import sys
+
+SIZE = 20
+MAX_LIMIT = 50
+MIN_LIMIT = -50
+# arr = [random.randint(MIN_LIMIT, MAX_LIMIT) for _ in range(SIZE)]
+arr = [37, -1, -47, 44, -14, -46, -24, 36, -28, 21, -1, 32, 23, 26, 20, 27, -19, -3, 47, 44]
+
+min_idx = None
+max_idx = None
+min_item = None
+max_item = None
+items_sum = None
+
+print(f"Исходный массив: {arr}")
+
+if len(arr) > 1:
+    # обходим массив, находим min и max элементы и их индексы
+    for idx, item in enumerate(arr):
+        if min_item is None or min_item > item:
+            min_item = item
+            min_idx = idx
+        elif max_item is None or max_item < item:
+            max_item = item
+            max_idx = idx
+
+    print(f"Минимальный элемент: '{min_item}', на позиции {min_idx}")
+    print(f"Максимальный элемент: '{max_item}', на позиции {max_idx}")
+
+    # если начальный индекс больше конечного, то меняем местами
+    if min_idx > max_idx:
+        min_idx, max_idx = max_idx, min_idx
+
+    # суммируем элементы между min и max лементами исключительно
+    for idx in range(min_idx+1, max_idx):
+        if items_sum is None:
+            items_sum = arr[idx]
+        else:
+            items_sum += arr[idx]
+
+    if items_sum is not None:
+        print(f"Сумма элементов между min и max = {items_sum}")
+    else:
+        print(f"min и max элементы находятся рядом")
+else:
+    print(f"Массив состоит из одного элемента")
+
+
+# анализ
+def check_item(key, value):
+    allowed_types = (str, list, dict, tuple, int, float,)
+    forbidden_items = (
+        key.startswith('__'),
+        not isinstance(value, allowed_types)
+    )
+    if any(forbidden_items):
+        return False
+    return True
+
+
+globals_snapshot = {key: value for key, value in globals().items() if check_item(key, value)}
+
+
+def show_size(obj, verbose=False):
+    total_size = 0
+    known_ids = set()
+
+    def _count_size(x, level=0):
+        _id = id(x)
+        if _id in known_ids:
+            if verbose:
+                print('\t' * level, f'{x} already exists (id={_id}), skipping')
+            return 0
+        known_ids.add(_id)
+
+        size = sys.getsizeof(x)
+
+        if verbose:
+            print('\t' * level, f'level->{level} type={type(x)}. size={size}, obj={x}, id={_id}')
+
+        if hasattr(x, '__iter__'):
+            if hasattr(x, 'items'):
+                for key, value in x.items():
+                    size += _count_size(key, level + 1)
+                    size += _count_size(value, level + 1)
+            elif not isinstance(x, str):
+                for i in x:
+                    size += _count_size(i, level + 1)
+        return size
+
+    for k, v in obj.items():
+        if verbose:
+            print(f'\ncalculating var {k}...')
+        total_size += _count_size(v)
+
+    return total_size
+
+
+vars_size = show_size(globals_snapshot)
+print(f'\n\nΣ = {vars_size} байт(а)')
+
+"""
+Python 3.6.7, x86_64
+
+Исходный массив: [37, -1, -47, 44, -14, -46, -24, 36, -28, 21, -1, 32, 23, 26, 20, 27, -19, -3, 47, 44]
+Минимальный элемент: '-47', на позиции 2
+Максимальный элемент: '47', на позиции 18
+Сумма элементов между min и max = 94
+
+
+Σ = 896 байт(а)
+"""
